73d23837f7ae5d6e17d5544de78b65b67ebc98a4
Anastassiya <anastassiya.belogurova@gmail.com>
2023-12-03 23:35:34 +0600

    Add fixtures

--- source/run.sh
+++ source/run.sh
@@ -2,8 +2,9 @@
 cd /app/
 python -m venv venv && source venv/bin/activate
 pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
-#for fixture in auth.json dump.json; do
-#  python manage.py loaddata fixtures/$$fixture
-#done
+for fixture in 01_auth.json 02_dump.json; do
+  python manage.py loaddata fixtures/$fixture
+done
 python manage.py runserver 0.0.0.0:8000
